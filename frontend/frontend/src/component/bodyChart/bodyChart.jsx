d9d976bf9cdd6687cf0795f2612f98a3fae8acc9
codebreaker0001 <yashyadav0171@gmail.com>
2024-03-11 22:59:16 +0530

    changes Done

---
+++ frontend/frontend/src/component/bodyChart/bodyChart.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+import './bodyChart.css'
+const BodyChart = () => {
+    
+    return(
+        <>
+
+        <div className='body-main'>
+            <img src="/humans.webp" alt="" />
+        </div>
+        </>
+    )
+};
+  
+export default BodyChart;
+  
