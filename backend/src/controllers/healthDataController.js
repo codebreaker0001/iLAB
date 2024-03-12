9a4ffdfe6aaa08e77e558dd873042926719e4664
Vaibhav Singh <the_vaibhav@icloud.com>
2024-03-13 00:06:34 +0530

    Data is now Fetched

--- backend/src/controllers/healthDataController.js
+++ backend/src/controllers/healthDataController.js
@@ -4,4 +4,4 @@ export const getHealthData = async (req, res)=>{
     const data = await HealthData.find(req.query);
     // console.log("data size is" , data.length)
     res.status(200).json(data);
-}
+} 
--- frontend/frontend/src/main.jsx
+++ frontend/frontend/src/main.jsx
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 import { Layout } from './layout.jsx'
-import { Route, Routes,RouterProvider , createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, Routes,RouterProvider , createBrowserRouter, createRoutesFromElements, Form } from 'react-router-dom'
 import CoverPage from './component/coverpage/CoverPage.jsx'
 import Home from './home/home.jsx'
 import LabReport from './component/labReport/LabReport.jsx'
 import { data } from './data.js'
 import BodyChart from './component/bodyChart/bodyChart.jsx'
-
+import Forms from "./component/Forms/Forms.jsx"
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(   
+
     <Route path='' element={<Layout/>}>
-      <Route path='/' element={<CoverPage data={data}/>}/>
+      <Route path='/' element={<Forms/>}/>
+      <Route path='/coverpage' element={<CoverPage data={data}/>}/>
       <Route path='/labreport' element={<LabReport data={data}/>}/>
       <Route path='/Coverpage' element={<CoverPage data={data}/>}/>     
       <Route path='/bodyChart' element={<BodyChart/>}/>     
@@ -25,7 +27,7 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+
   <RouterProvider router={router}/>
   </React.StrictMode>,
 )
