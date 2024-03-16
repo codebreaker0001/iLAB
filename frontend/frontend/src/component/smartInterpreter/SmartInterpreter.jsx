22a50756b95c094ee2a37809b3b5a0e281088950
Kavyansh Pancholi <kavyanshpancholi@gmail.com>
2024-03-17 02:25:20 +0530

    Fetching Data for VisuallyAid and Smart Interpreter fixed

--- frontend/frontend/src/component/smartInterpreter/SmartInterpreter.jsx
+++ frontend/frontend/src/component/smartInterpreter/SmartInterpreter.jsx
@@ -3,51 +3,28 @@ import React from "react";
 import gen from "./GenerateInt";
 import "./Interpreter.css";
 import { useSelector } from "react-redux";
+import { aiGeneratedForSmartInt } from "../../store/dataSlice";
+import Loading from "../loader/Loading";
 
 const SmartInterpreter = () => {
   const p  = useSelector(state=>state.data);
   const data = p[1];
-  console.log(data);
-
-    if(!data) return <div>Loading...</div>;
+  // console.log(data);
+  // if(!data) return <div>Loading...</div>;
     
-  const arr = [];
-  for (let i = 0; i < data?.length; i++) {
-    for (let j = 0; j < data[i].test_values.length; j++) {
-      if (data[i].test_values[j].is_highlighted) {
-        arr.push(data[i].test_values[j]);
-      }
-    }
-  }
-  const res = [];
-  for (let i = 0; i < Math.min(15, arr.length); i++) {
-    if (arr[i]?.lower_bound == "-" || arr[i]?.lower_bound == "") {
-      continue;
-    }
-    if (arr[i].parameter_value < arr[i].lower_bound) {
-      res.push(["deficiency", arr[i].parameter_name]);
-    } else if (arr[i].parameter_value > arr[i].upper_bound) {
-      res.push(["High level", arr[i].parameter_name]);
-    } else {
-      res.push(["", arr[i].parameter_name]);
-    }
-  }
+  
   const [result, setResult] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const para = await gen(res);
-        setResult(para);
-        console.log(para);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
+    setResult(aiGeneratedForSmartInt);
+    if (aiGeneratedForSmartInt.length) {
+      setLoading(false);
+    }
   }, []);
   let i = 0;
-  return (
+  return (<>{isLoading ? (<Loading />) : (
     <div>
       {data.map((tests) => {
         return (
@@ -111,6 +88,8 @@ const SmartInterpreter = () => {
         );
       })}
     </div>
+  )
+    }</>
   );
 };
 export default React.memo(SmartInterpreter);
