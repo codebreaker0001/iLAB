9167ca687ca46af5dff0f134f459ba7262ab024a
Vaibhav Singh <119793533+DiloKaaShooter@users.noreply.github.com>
2024-03-20 23:03:58 +0530

    proxy changed

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      '/api': "http://localhost:4000",
+      '/api': "https://ilab-mk18.onrender.com",
     }
   },
   plugins: [react()],
